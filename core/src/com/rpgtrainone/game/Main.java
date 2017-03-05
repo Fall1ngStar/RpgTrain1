7f87f03faa55635a439f9192e78ddc96167889fe
Thierry GARCIN <thierry.garcin@etu.univ-lyon1.fr>
2017-03-05 12:54:38 +0100

    Ajout déplacement image de base

--- core/src/com/rpgtrainone/game/Main.java
+++ core/src/com/rpgtrainone/game/Main.java
@@ -3,31 +3,79 @@ package com.rpgtrainone.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	BitmapFont font;
+	ShapeRenderer sr;
+	OrthographicCamera camera;
+	int width;
+	int height;
+	int x;
+	int y;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		x = 0;
+		y = 0;
+		width = Gdx.app.getGraphics().getWidth();
+		height = Gdx.app.getGraphics().getHeight();
+		font = new BitmapFont();
+		sr = new ShapeRenderer();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,width,height);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+		batch.setProjectionMatrix(camera.combined);
+        sr.setProjectionMatrix(camera.combined);
+
+		updatePos();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x-img.getWidth()/2, y-img.getHeight()/2);
 		batch.end();
+
+        sr.begin(ShapeRenderer.ShapeType.Line);
+        sr.setColor(1,0,0,1);
+        sr.line(1180,900,1220,900);
+        sr.line(1200,880,1200,920);
+        sr.end();
+
+        batch.begin();
+        font.getData().setScale(2);
+        font.setColor(1,1,1,1);
+        String inputPos = "x: " + Gdx.input.getX() + " y: " + Gdx.input.getY();
+        font.draw(batch, inputPos,width/2,height/2);
+        batch.end();
 	}
 	
 	@Override
 	public void dispose () {
+	    sr.dispose();
 		batch.dispose();
 		img.dispose();
 	}
+
+	public void updatePos(){
+	    if(Gdx.input.isTouched()){
+	        x = (x + Gdx.input.getX())/2;
+            y = (y + (height - Gdx.input.getY()))/2;
+	    }
+    }
+
+
 }
